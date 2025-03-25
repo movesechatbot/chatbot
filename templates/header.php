4ec4a633c3372a3ad3b9b43432de2d1c97e6238d
TheoTavora <theovannitavora@gmail.com>
2025-03-25 18:45:24 -0300

    NEW: Header

---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/CSS/indexheader.css" />
+    <title>Header</title>
+</head>
+<header>
+    <a href="#">
+        <img src="../static/imgs/logopawfoliomenor.png" alt="logo" />
+    </a>
+    <ul class="nav-links">
+        <li class="nav-item"><a href="/templates/index.php">HOME</a></li>
+        <li class="nav-item"><a href="#">SERVIÇOS</a></li>
+        <li class="nav-item"><a href="#">AGENDAMENTOS</a></li>
+        <a href="/templates/Login.php" class="header-button">LOGIN</a>
+        <a href="/templates/cadastro.php" class="header-button">CADASTRAR</a>
+    </ul>
+</header>
+<body>
+</body>
+</html>
