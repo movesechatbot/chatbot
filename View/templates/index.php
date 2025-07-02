4afd2d11af667838cbd39a42fa0261e3fe0c6d99
TheoTavora <theovannitavora@gmail.com>
2025-07-01 23:09:48 -0300

    ADD: FAQ Button on index page NEW: Chatbot template

--- View/templates/index.php
+++ View/templates/index.php
@@ -23,6 +23,7 @@ if (isset($_SESSION['user'])) {
     <link rel="stylesheet" type="text/css" href="../static/CSS/index.css" />
     <!--ICON-->
     <link rel="icon" type="image/png" href="../static/imgs/logopawfoliomenor.png"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <!--BOOTSTRAP CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
@@ -185,6 +186,7 @@ if (isset($_SESSION['user'])) {
             </div>
         </footer> 
         -->
+        <a href="chatbot.html" class="chatbot"><i class="fa-solid fa-comments"></i></a>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
