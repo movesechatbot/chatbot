604bb379d1429308aa38db0dcbeb01560026daaf
lucasogoncalves <oliveira.lgoncalves@gmail.com>
2025-10-15 13:54:11 -0300

    Add Creci e explicação do minha casa minha vida

--- llm.py
+++ llm.py
@@ -69,6 +69,7 @@ def ask_chatgpt(
         "sempre Separe frases com mais de 4 palavras pulando duas linhas para facilitar a leitura."
         "Nunca antecipe informações de outras etapas."
         "Nunca encerre o assunto e nunca prometa retorno futuro."
+        "Esse é o nosso Creci:\n\n28339 J - Imobiliária Move.se Gênesis LTDA"
         "Sua única meta é conduzir o lead pelo fluxo de atendimento até a etapa CONTEXTUALIZAÇÃO."
         "Use o histórico apenas para manter coerência."
         "Nunca invente, nunca ofereça ajuda extra e nunca fale sobre equipe, valores ou horários"
