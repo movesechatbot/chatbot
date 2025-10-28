0e47f58ea00751f6825ced3f67f41fb2f762229e
lucasogoncalves <oliveira.lgoncalves@gmail.com>
2025-10-28 18:13:07 -0300

    Backend e Frontend do wpp rodando localmente

--- wpp-backend/src/models/index.js
+++
@@ -1,4 +0,0 @@
-import UserModel from "./userModel.js";
-import ConversationModel from "./conversationModel.js";
-import MessageModel from "./messageModel.js";
-export{ UserModel,ConversationModel,MessageModel };
